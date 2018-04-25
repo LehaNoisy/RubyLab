2688fd5f5fad60fdba580b5b94f0b301dc198d98
LehaNoisy <shumilovy@mail.ru>
2018-04-25 14:30:28 +0300

    task1

---
+++ task1/task1_changed.rb
@@ -0,0 +1,12 @@
+
+def task1(input)
+
+string.each_line do |line|
+  line if (line['error'])
+end
+
+string.each_line do |line|
+  string.split('[')[1].split(']')[0] + " FROM:" + string.split[0] + " TO:" + string.upcase.split('POST')[1].split('HTTP')[0]
+end
+
+end
